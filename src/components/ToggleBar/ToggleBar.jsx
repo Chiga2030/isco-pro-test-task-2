1cf0ddba516a8030ebf6fec0b6ca25b9cafc42e8
Anton <chiga2030@gmail.com>
2022-01-30 15:51:38 +0300

    rebased click handler to separete file

--- src/components/ToggleBar/ToggleBar.jsx
+++ src/components/ToggleBar/ToggleBar.jsx
@@ -6,6 +6,10 @@ import {
   useRef,
 } from 'react';
 
+import {
+  onDetectAndSetNewActivePositionHandler,
+} from './utils';
+
 import ActiveArea from './ActiveArea';
 
 const buttons = [
@@ -27,6 +31,13 @@ const ToggleBar = () => {
 
   const buttonsWrapper = useRef(null);
 
+  const onClickButtonHandler = onDetectAndSetNewActivePositionHandler.bind(
+    null,
+    position.centerPosition,
+    setPosition,
+    buttonsWrapper.current,
+  );
+
   useEffect(() => {
     setPosition({
       centerPosition: {
@@ -43,45 +54,6 @@ const ToggleBar = () => {
   }, []);
 
 
-  const onDetectAndSetNewActivePositionHandler = button => {
-    const widthButton = Math.ceil(
-      window.getComputedStyle(button, null).width.match(/\d+.+\d/));
-
-    const currentWidth = Math.ceil(
-      position.centerPosition.width.match(/\d+./));
-
-    const newScale = Math.round((widthButton / currentWidth) * 100) / 100 ;
-
-    setPosition({
-      leftPosition: {
-        transform: `translateX(${
-          button.offsetLeft}px)`,
-      },
-      centerPosition: {
-        ...position.centerPosition,
-        transform: `translateX(calc(${
-          button.offsetLeft}px + .75rem)) scaleX(${newScale})`,
-      },
-      rightPosition: {
-        transform: `translateX(calc(${
-          button.offsetLeft + widthButton}px + .7rem))`,
-      },
-    });
-
-    setButtonsColor(buttonsWrapper.current.children, button);
-  };
-
-  const setButtonsColor = (buttons, activeButton) => {
-    Array.prototype.forEach.call(
-      buttons,
-      element => element.style.color = 'var(--text-color-main)');
-
-    setTimeout(() => {
-      activeButton.style.color = 'var(--text-color-light)';
-    }, 200);
-  };
-
-
   return (
     <div className={ styles.ToggleBar }>
       <div className={ styles.buttonBar }>
@@ -89,8 +61,8 @@ const ToggleBar = () => {
           { buttons.map(button => (
             <Button
               key={ button }
-              onDetectAndSetNewActivePositionHandler={
-                onDetectAndSetNewActivePositionHandler }
+              onClickButtonHandler={
+                onClickButtonHandler }
               textButton={ button }
             />
           )) }
@@ -107,7 +79,7 @@ const ToggleBar = () => {
 
 
 const Button = ({
-  onDetectAndSetNewActivePositionHandler,
+  onClickButtonHandler,
   textButton,
 }) => {
   const button = useRef(null);
@@ -118,7 +90,7 @@ const Button = ({
       type="button"
       className={ styles.button }
       value={ textButton }
-      onClick={ () => onDetectAndSetNewActivePositionHandler(button.current) }
+      onClick={ () => onClickButtonHandler(button.current) }
     />
   );
 };
